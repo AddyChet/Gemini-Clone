397ca53187e6c886937c17a30274db290f2759b6
AddyChet <adarshbhandari1002@gmail.com>
2025-07-01 01:44:28 +0530

    modified the api, installed openAI

--- src/config/ai.js
+++ src/config/ai.js
@@ -1,16 +1,13 @@
-import { InferenceClient } from "@huggingface/inference";
-
-const client = new InferenceClient(import.meta.env.VITE_HF_TOKEN);
-
-// Helper to remove <think>...</think> block
-function removeThinkBlock(text) {
-  return text.replace(/<think>[\s\S]*?<\/think>/gi, "").trim();
-}
+import OpenAI from "openai";
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: import.meta.env.VITE_OPEN_AI_API,
+  dangerouslyAllowBrowser: true,
+});
 
 export async function generate(prompt) {
-  const chatCompletion = await client.chatCompletion({
-    provider: "fireworks-ai",
-    model: "deepseek-ai/DeepSeek-R1-0528",
+  const completion = await openai.chat.completions.create({
+    model: "deepseek/deepseek-r1:free",
     messages: [
       {
         role: "user",
@@ -19,9 +16,7 @@ export async function generate(prompt) {
     ],
   });
 
-  let result = chatCompletion.choices[0].message.content;
-
-  // Cleaning the <think> block from result
-  result = removeThinkBlock(result);
+  console.log(completion.choices[0].message);
+  const result = completion.choices[0].message.content;
   return result;
 }
