5f13902992988080429db801511395516c397aab
AddyChet <adarshbhandari1002@gmail.com>
2025-07-01 21:02:07 +0530

    added mistral ai for image reading

--- src/config/ai.js
+++ src/config/ai.js
@@ -17,4 +17,35 @@ export async function generate(prompt) {
   });
   const result = completion.choices[0].message.content;
   return result;
+
+  
+}
+
+
+export async function generateImage(prompt, url) {
+  const completion = await openai.chat.completions.create({
+    model: "mistralai/mistral-small-3.2-24b-instruct:free",
+    messages: [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": prompt
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": url
+            }
+          }
+        ]
+      }
+    ],
+    
+  });
+
+  console.log(completion.choices[0].message);
+  const result = completion.choices[0].message.content
+  return result;
 }
