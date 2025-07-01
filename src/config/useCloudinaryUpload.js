3c824f7b62bc2d33dfdd345b7913ca4b622d5880
AddyChet <adarshbhandari1002@gmail.com>
2025-07-01 21:23:40 +0530

    fixed the cloudName which was undefined

--- src/config/useCloudinaryUpload.js
+++ src/config/useCloudinaryUpload.js
@@ -1,5 +1,7 @@
 export async function uploadToCloudinary(file) {
-  const url = `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/upload`; // replace <cloud_name>
+  const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+  console.log(CLOUD_NAME)
+  const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload`; // replace <cloud_name>
   const formData = new FormData();
   formData.append("file", file);
   formData.append("upload_preset", "unsigned_upload"); // replace <upload_preset>
