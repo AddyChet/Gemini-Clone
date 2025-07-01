69d8278971ef08f3d7dcdf8be14d3cb645c4940a
AddyChet <adarshbhandari1002@gmail.com>
2025-07-01 21:15:43 +0530

    css + dependencies changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,         // enables external access for LAN preview
+    port: 5173          // optional: set a fixed port
+  },
 })
